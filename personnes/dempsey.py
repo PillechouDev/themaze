8856e585a9d7df178a39cc9ce9511fdaf79bdfb4
Rhummboy <romainpanii@gmail.com>
2020-10-25 17:11:08 +0100

    Commentaire dempsey

--- personnes/dempsey.py
+++ personnes/dempsey.py
@@ -20,6 +20,7 @@ class Dempsey (Personnage):
 
     def parler(self, joueur):
         sac = joueur.getSac()
+        #On fait une condition pour savoir si le sac est vide afin de ne pas boucler sur du vide
         if (len(sac)) == 0:
             print("Dempsey : tiens j'ai trouvé quelque chose qui pourrait etre utile camarade")
             entree = input("#> Voulez vous prendre l'objet de forme sphérique qu'il vous tend ? [o/n]")
@@ -29,6 +30,7 @@ class Dempsey (Personnage):
             else:
                 print("vous avez refusé l'objet")
         else:
+            #si le sac n'est pas vide, on fait une boucle pour savoir si la clé d'invocation est déjà présente
             for obj in sac:
                 if obj.description() == "ClefInvocation":
                     print("Dégage !! je t'ai deja donné ce que j'avais")
