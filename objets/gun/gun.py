adc0643d6909c2d415805a8875dfa152bef25d69
Mikadal <axel.danglot@epsi.fr>
2020-12-06 16:15:09 +0100

    equipement arme

--- objets/gun/gun.py
+++ objets/gun/gun.py
@@ -1,4 +1,5 @@
 from exceptions import AbstractMethodCallException
+from joueur import Joueur
 """
 Class mère Gun représente un objet que l'on peut ramasser et que le joueur peut transporter et utiliser à partir de son sac
 
@@ -23,13 +24,17 @@ class Gun:
         raise AbstractMethodCallException() # Méthode abstraite
 
     def utiliser(self, joueur):
+        joueur = Joueur()
         """ Cette méthode est appelée lorsque le joueur utilise un objet. """
-        raise AbstractMethodCallException() # Méthode abstraite
+        choix = input("souhaitez-vous equiper l'arme [o/n]")
+        if choix in ['o', 'O', 'OUI', 'oui']:
+            joueur.degat = joueur.degat + self.damage
 
     def getDamage(self, joueur):
+        joueur = Joueur()
         """ Verifie si le joueur à l'atout double tap qui permet de doubler les dégats """
         for t in joueur.getAtout():
             if t == "doubleTap":
-                return self._damage * 2
+                return self._damage * 2 + joueur.degat
             else:
-                return self._damage
+                return self._damage + joueur.degat
